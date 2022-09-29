66ed97b0d6c537ad6279ee28644181f1c0373ee7
Adeniran John <adeniranjohn2016@gmail.com>
2022-09-29 16:06:41 +0100

    Updated convert model

--- custom_admin/forms.py
+++ custom_admin/forms.py
@@ -68,10 +68,21 @@ class ConvertForm(forms.ModelForm):
                 Column('email', css_class='form-group col-md-6 mb-0'),
                 css_class='form-row'
             ),
-            'address',
+            'linkedin',
+            Row(
+                Column('address', css_class='form-group col-md-9 mb-0'),
+                Column('city', css_class='form-group col-md-3 mb-0'),
+                css_class='form-row'
+            ),
+            Row(
+                Column('state', css_class='form-group col-md-6 mb-0'),
+                Column('region', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
             'category',
             'school',
             'denomination',
+            
             Submit('submit', 'Register Convert')
         )
 

bd3f8a90bdc9dd62582321781b3158692c69bff8
Adeniran John <adeniranjohn2016@gmail.com>
2023-02-18 10:08:10 +0100

    added move to newcomer action

--- custom_admin/admin.py
+++ custom_admin/admin.py
@@ -14,7 +14,7 @@ class RegionAdmin(admin.ModelAdmin):
 class StateAdmin(admin.ModelAdmin):
     list_display = ('id', 'state')
     
-def move_to_convert(modeladmin, request, queryset):
+def move_participant_to_convert(modeladmin, request, queryset):
     for participant in queryset:
         convert = Convert.objects.create(
             first_name=participant.first_name,
@@ -33,19 +33,44 @@ def move_to_convert(modeladmin, request, queryset):
             denomination=participant.denomination
         )
         convert.save()
-        messages.success(request, f"{len(queryset)} converts moved to convert.")
+    messages.success(request, f"{len(queryset)} participants moved to convert.")
 
+def move_participant_to_newcomer(modeladmin, request, queryset):
+    for participant in queryset:
+        newcomer = Newcomer.objects.create(
+            first_name=participant.first_name,
+            last_name=participant.last_name,
+            gender=participant.gender,
+            age=participant.age,
+            phone_no=participant.phone_no,
+            email=participant.email,
+            linkedin=participant.linkedin,
+            address=participant.address,
+            city=participant.city,
+            state=participant.state,
+            region=participant.region,
+            category=participant.category,
+            school=participant.school,
+            denomination=participant.denomination
+        )
+        newcomer.save()
+    messages.success(request, f"{len(queryset)} participants moved to newcomer.")
 
 @admin.register(Participant)
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'gender', 'region', 'state')
     list_filter = ('gender', 'state', 'region', 'category', 'denomination', 'registered_date')
-    actions = [move_to_convert, ]
+    actions = [move_participant_to_convert, move_participant_to_newcomer]
     
     def move_to_convert(self, obj):
-        return mark_safe('<a class="button" href="{}">Move the convert</a>',
+        return mark_safe('<a class="button" href="{}">Move to convert</a>',
                            reverse('admin:move-participant-to-convert') + f"?ids={obj.id}")
-    move_to_convert.short_description = "Move selected to convert"    
+    move_to_convert.short_description = "Move selected to convert"
+    
+    def move_to_newcomer(self, obj):
+        return mark_safe('<a class="button" href="{}">Move to newcomer</a>',
+                           reverse('admin:move-participant-to-newcomer') + f"?ids={obj.id}")
+    move_to_newcomer.short_description = "Move selected to newcomer"
     
 @admin.register(Convert)
 class ConvertAdmin(admin.ModelAdmin):
